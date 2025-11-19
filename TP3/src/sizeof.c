f199185e1afc23dfd87ef4e27bfcadd5b179f08f
Douae1230 <dodo159@eleve.isep.fr>
2025-11-19 16:47:25 +0100

    Update sizeof.c

--- TP3/src/sizeof.c
+++ TP3/src/sizeof.c
@@ -12,18 +12,21 @@ int main() {
     printf("La taille de long long est : %zu octets\n", sizeof(long long));
     printf("La taille de long double est : %zu octets\n", sizeof(long double));
 
-    /* Pointeurs simples */
+    /* Pointeurs */
     printf("\nPointeurs simples :\n");
     printf("La taille de int* est : %zu octets\n", sizeof(int*));
-    printf("La taille de int** est : %zu octets\n", sizeof(int**));
     printf("La taille de char* est : %zu octets\n", sizeof(char*));
-    printf("La taille de char** est : %zu octets\n", sizeof(char**));
-    printf("La taille de char*** est : %zu octets\n", sizeof(char***));
     printf("La taille de float* est : %zu octets\n", sizeof(float*));
+    printf("La taille de double* est : %zu octets\n", sizeof(double*));
+
+    /* Pointeurs multiples */
+    printf("\nPointeurs multiples :\n");
+    printf("La taille de int** est : %zu octets\n", sizeof(int**));
+    printf("La taille de char** est : %zu octets\n", sizeof(char**));
     printf("La taille de float** est : %zu octets\n", sizeof(float**));
-    printf("La taille de float*** est : %zu octets\n", sizeof(float***));
+    printf("La taille de double** est : %zu octets\n", sizeof(double**));
+    printf("La taille de int*** est : %zu octets\n", sizeof(int***));
+    printf("La taille de char*** est : %zu octets\n", sizeof(char***));
 
     return 0;
 }
-
-
